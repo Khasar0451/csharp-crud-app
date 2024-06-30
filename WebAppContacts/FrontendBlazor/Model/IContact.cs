9af5586f67686ee91dd3f39e49df75de6eb66a4c
Executed419 <lucker10@interia.pl>
2024-06-30 21:20:15 +0200

    started blazor (another frontend) project

---
+++ WebAppContacts/FrontendBlazor/Model/IContact.cs
@@ -0,0 +1,16 @@
+﻿namespace FrontendBlazor.Model;
+
+public class IContact
+{
+    public int id { get; set; }
+    public required string name { get; set; }
+    public required string surname { get; set; }
+    public required string email { get; set; }
+    public required string password { get; set; }
+    public int contactCategoryId { get; set; }
+    public required string contactCategory { get; set; }
+    public int contactSubategoryId { get; set; }
+    public required string contactSubategory { get; set; }
+    public int  phoneNumber { get; set; }
+    public DateOnly birthdate { get; set; }
+}
